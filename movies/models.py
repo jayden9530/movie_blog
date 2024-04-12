fdcf472f68efed7676ddb17105232445510ea3dd
= <=>
2024-04-12 09:52:31 +0900

    Add is_open field in Movie Model

--- movies/models.py
+++ movies/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # 클래스명은 테이블 이름 
 class Movie(models.Model):
     # 각 변수값은 필드명 
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     content = models.TextField()
-
+    is_open = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
